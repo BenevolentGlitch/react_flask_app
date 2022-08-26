70501647c9f76b4c0dc044455c38fd2429bcabb0
BenevolentGlitch <purnellanthony@gmail.com>
2022-08-26 22:10:09 +0000

    Update api.py

--- api/api.py
+++ api/api.py
@@ -3,12 +3,12 @@ from flask import Flask, render_template, request
 
 from handlers import upload_csv_handler
 
+
 app = Flask(__name__)
 app.config.from_pyfile('config.py')
 
 GET = 'GET'
 POST = 'POST'
-
 
 @app.route("/")
 def index():
